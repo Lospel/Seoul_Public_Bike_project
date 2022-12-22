814dea43df1fcafb820c5d215a44ee6107e7c43d
Lospel <rlaehdnl@gmail.com>
2022-12-22 16:00:48 +0900

    updated

--- bike/src/main/java/com/example/demo/user/UserCreateForm.java
+++ bike/src/main/java/com/example/demo/user/UserCreateForm.java
@@ -9,6 +9,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserCreateForm {
+
+    @NotEmpty(message = "이름은 필수항목입니다.")
+    private String nickname;
+
     @Size(min = 3, max = 25)
     @NotEmpty(message = "사용자ID는 필수항목입니다.")
     private String username;
@@ -22,4 +26,5 @@ public class UserCreateForm {
     @NotEmpty(message = "이메일은 필수항목입니다.")
     @Email
     private String email;
+
 }
