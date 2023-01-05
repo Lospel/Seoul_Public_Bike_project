66fdc5789eaabd036fff4081bd875731b6061712
Lospel <rlaehdnl@gmail.com>
2023-01-05 16:10:12 +0900

    up

--- bike/src/main/java/com/example/demo/question/Question.java
+++ bike/src/main/java/com/example/demo/question/Question.java
@@ -48,8 +48,7 @@ public class Question {
     
     @Column(columnDefinition = "integer default 0", nullable = false)
     private int view;
-
+    
     private String fileName;
     private String filePath;
-    // private String fileOriName;
 }
