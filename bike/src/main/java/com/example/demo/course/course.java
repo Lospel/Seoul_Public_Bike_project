544f3f72b588e8f75eb6ee68e9dacf5568583605
Moon-999 <munhui3078@naver.com>
2023-01-10 11:07:39 +0900

    update

--- bike/src/main/java/com/example/demo/course/course.java
+++ bike/src/main/java/com/example/demo/course/course.java
@@ -1,44 +1,32 @@
 package com.example.demo.course;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
 public class course {
-   @Id
-   private String id;
+   @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer id;
    
-   private String c1_1;
-
-   private String c1_2;
+   private String name;
    
-   private String c1_3;
+   private String longitude;
 
-   private String c2_1;
-
-   private String c2_2;
-
-   private String c2_3;
-
-   private String c3_1;
-
-   private String c3_2;
-
-   private String c3_3;
-
-   private String c4_1;
-
-   private String c4_2;
-
-   private String c4_3;
-
-   private String c5_1;
-
-   private String c5_2;
-
-   private String c5_3;
+   private String latitude;
+   
+   private LocalDateTime createDate;
 }
