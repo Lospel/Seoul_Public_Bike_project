d0e07ca09b2d2c2cd477a131ddf5106629ecbe27
Lospel <rlaehdnl@gmail.com>
2023-01-16 10:07:26 +0900

    OpenWeather Api

--- bike/src/main/resources/static/js/OpenWeather.js
+++ bike/src/main/resources/static/js/OpenWeather.js
@@ -23,12 +23,12 @@ $.ajax({
         var $Icon = (resp.weather[0].icon).substr(0,2);
         var $weather_description = resp.weather[0].main;
         var $Temp = Math.floor(resp.main.temp- 273.15) + 'º';
-        var $humidity = '습도&nbsp;&nbsp;&nbsp;&nbsp;' + resp.main.humidity+ ' %';
-        var $wind = '바람&nbsp;&nbsp;&nbsp;&nbsp;' +resp.wind.speed + ' m/s';
-        var $city = '서울';
-        var $cloud = '구름&nbsp;&nbsp;&nbsp;&nbsp;' + resp.clouds.all +"%";
-        var $temp_min = '최저 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_min- 273.15) + 'º';
-        var $temp_max = '최고 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_max- 273.15) + 'º';
+        var $humidity = 'Humidity&nbsp;&nbsp;&nbsp;&nbsp;' + resp.main.humidity+ ' %';
+        var $wind = 'Wind&nbsp;&nbsp;&nbsp;&nbsp;' +resp.wind.speed + ' m/s';
+        var $city = 'Seoul';
+        var $cloud = 'Cloud&nbsp;&nbsp;&nbsp;&nbsp;' + resp.clouds.all +"%";
+        var $temp_min = 'Lowest Temperature&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_min- 273.15) + 'º';
+        var $temp_max = 'Highest Temperature&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_max- 273.15) + 'º';
         
 
         $('.weather_icon').append('<i class="' + weatherIcon[$Icon] +' fa-5x" style="height : 150px; width : 150px;"></i>');
